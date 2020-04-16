40f59f4c19d62c6e62686ff8925d9253c4fdf653
Machiko Lacey <machiko_kimura_lacey@yahoo.co.uk>
2020-04-16 23:39:15 +0100

    Updates

---
+++ precache-manifest.5db2d0bd87779df6a6336df7311ac841.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83cea703d88f492f82ce7b5e9ec5cdb",
+    "url": "/react-movie-list/index.html"
+  },
+  {
+    "revision": "82df670533f460885331",
+    "url": "/react-movie-list/static/css/2.6492506f.chunk.css"
+  },
+  {
+    "revision": "22c5f89ad17a8eac66c7",
+    "url": "/react-movie-list/static/css/main.3a3aeab8.chunk.css"
+  },
+  {
+    "revision": "82df670533f460885331",
+    "url": "/react-movie-list/static/js/2.ae097bab.chunk.js"
+  },
+  {
+    "revision": "dc18aa49cef6030bc6f21416e2f19f12",
+    "url": "/react-movie-list/static/js/2.ae097bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c5f89ad17a8eac66c7",
+    "url": "/react-movie-list/static/js/main.0f2961f4.chunk.js"
+  },
+  {
+    "revision": "4d96055d1a2ea085e376",
+    "url": "/react-movie-list/static/js/runtime-main.87e983ed.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-movie-list/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-movie-list/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-movie-list/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-movie-list/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-movie-list/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
